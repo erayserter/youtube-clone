90efb50033f8663fbda521279b5b4b0a7a11da42
Mehmet Eray Serter <mehmeterayserter@gmail.com>
2024-01-11 05:32:52 +0300

    video processing information now added to db.

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -8,6 +8,7 @@ import {
   convertVideo,
   setupDirectories,
 } from "./storage";
+import { isVideoNew, setVideo } from "./firestore";
 
 setupDirectories();
 
@@ -34,6 +35,19 @@ app.post("/process-video", async (req, res) => {
 
   const inputFileName = data.name;
   const outputFileName = `processed-${inputFileName}`;
+  const videoId = inputFileName.split(".")[0];
+
+  if (!isVideoNew(videoId)) {
+    return res
+      .status(400)
+      .send("Bad Request: video already processing or processed.");
+  } else {
+    await setVideo(videoId, {
+      id: videoId,
+      uid: videoId.split("-")[0],
+      status: "processing",
+    });
+  }
 
   // Download the raw video from Cloud Storage
   await downloadRawVideo(inputFileName);
@@ -51,6 +65,11 @@ app.post("/process-video", async (req, res) => {
 
   // Upload the processed video to Cloud Storage
   await uploadProcessedVideo(outputFileName);
+
+  await setVideo(videoId, {
+    status: "processed",
+    filename: outputFileName,
+  });
 
   await Promise.all([
     deleteRawVideo(inputFileName),
